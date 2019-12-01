db8e6b125b582d217cb00e2c56047b750b8941a8
Wryn Wagner <wagner.wryn@gmail.com>
2019-11-30 20:41:45 -0700

    Added docking/undocking shortcuts

---
+++ config/xserver/screenlayout/homedock_hdmionly.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 3840x2160 --pos 2880x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
---
+++ config/xserver/screenlayout/undock.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --primary --mode 2880x1800 --scale 0.7x0.7 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --off
