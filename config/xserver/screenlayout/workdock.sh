4ae4732410185c33db1298a25b30dd3d2d772eb0
Wryn Wagner <wagner.wryn@gmail.com>
2019-12-02 12:36:26 -0700

    Updated broken dock script

--- config/xserver/screenlayout/workdock.sh
+++ config/xserver/screenlayout/workdock.sh
@@ -23,13 +23,13 @@ elif [ $ROLL -eq 1 ]; then
 		>/dev/null 2>&1
 elif [ $ROLL -eq 2 ]; then
 	feh --bg-fill\
-		$HOME/Pictures/bg/roy.jpg\
 		$HOME/Pictures/bg/fe8.jpg\
 		$HOME/Pictures/bg/micaiah.jpg\
+		$HOME/Pictures/bg/roy.jpg\
 		>/dev/null 2>&1
 elif [ $ROLL -eq 3 ]; then
 	feh --bg-fill\
-		$HOME/Pictures/bg/chrom.jpg\
+		$HOME/Pictures/bg/chrom.png\
 		$HOME/Pictures/bg/byleth.jpg\
 		$HOME/Pictures/bg/link.jpg\
 		>/dev/null 2>&1
