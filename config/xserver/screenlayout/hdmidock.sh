5f3e4d20f7bf7c6af69970446868206cf139342a
Wryn Wagner <wagner.wryn@gmail.com>
2020-01-10 09:03:16 -0700

    Edited HDMIDOCK script to utilize hdmi default

--- config/xserver/screenlayout/hdmidock.sh
+++ config/xserver/screenlayout/hdmidock.sh
@@ -4,7 +4,7 @@ xrandr --output DP-0 --off\
 	   --output DP-3 --off\
 	   --output DP-4 --off\
 	   --output DP-2 --primary --mode 2880x1800 --scale 0.8x0.8 --pos 0x0 --rotate normal\
-	   --output HDMI-0 --mode 1920x1080 --pos 2880x0 --rotate normal
+	   --output HDMI-0 --pos 2880x0 --rotate normal
 
 ROLL=$(($RANDOM%4));
 
