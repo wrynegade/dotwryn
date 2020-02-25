7e8ce8a3e0b602186d6c209e029772f2f5e1531f
Wryn Wagner <wagner.wryn@gmail.com>
2020-02-24 18:31:37 -0700

    updated for new monitor setup at work

--- config/xserver/screenlayout/workdock.sh
+++ config/xserver/screenlayout/workdock.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 if xrandr | grep -q "DP-0 connected"; then
 	xrandr \
-		--output DP-0 --mode 1920x1080 --pos 0x0    --rotate normal --scale 1.5x1.5 --primary\
+		--output DP-0 --mode 1920x1080 --pos 0x0    --rotate left --scale 1.5x1.5 --primary\
 		--output DP-1 --off\
 		--output DP-2 --mode 2880x1800 --pos 5760x0 --rotate normal --scale 0.8x0.8\
 		--output DP-3 --off\
-		--output DP-4 --mode 1920x1080 --pos 2880x0 --rotate normal --scale 1.5x1.5\
+		--output DP-4 --mode 1920x1080 --pos 2880x0 --rotate right --scale 1.5x1.5\
 		--output HDMI-0 --off\
+		--setmonitor BigBoi auto DP-0,DP-4\
 	;
 else
 	xrandr \
 		--output DP-0 --off\
 		--output HDMI-0 --off\
-		--output DP-1 --mode 1920x1080 --pos 0x0    --rotate normal --scale 1.5x1.5 --primary\
+		--output DP-1 --mode 1920x1080 --pos 0x0    --rotate left --scale 1.5x1.5 --primary\
 		--output DP-2 --mode 2880x1800 --pos 5760x0 --rotate normal --scale 0.8x0.8\
-		--output DP-3 --mode 1920x1080 --pos 2880x0 --rotate normal --scale 1.5x1.5\
+		--output DP-3 --mode 1920x1080 --pos 2880x0 --rotate right --scale 1.5x1.5\
 		--output DP-4 --off\
+		--setmonitor BigBoi auto DP-1,DP-3\
 	;
 fi
 
