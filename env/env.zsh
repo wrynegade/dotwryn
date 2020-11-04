6c819ad7941579e7874ddde3c68e64ce4acf34b6
Wryn Wagner <wagner.wryn@gmail.com>
2020-11-04 14:14:19 -0700

    support for default PREFERRED_EDITOR

--- env/env.zsh
+++ env/env.zsh
@@ -4,6 +4,9 @@ RD_PATH="$HOME/RentDynamics"
 SCHOOL_DIR="$HOME/School"
 DOTWRYN="$HOME/.wryn"
 
+# ordered from least-preferred to most-preferred
+PREFERRED_EDITOR=(vi vim)
+
 LOCAL_OVERRIDES="$HOME/.config/wryn/env.zsh"
 [ -f "$LOCAL_OVERRIDES" ] \
 	&& source "$LOCAL_OVERRIDES" \
