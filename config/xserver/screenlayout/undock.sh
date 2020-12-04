970b6bcc33f52825b4065a6d3fa1dddfb6b6958e
Wryn Wagner <wagner.wryn@gmail.com>
2020-12-04 13:02:51 -0700

    Updated undock to toggle between 4k and 2k resolutions

--- config/xserver/screenlayout/undock.sh
+++ config/xserver/screenlayout/undock.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 NATIVE_MONITOR='eDP1';
+NATIVE_MODE="$(xrandr | grep -A 2 "$NATIVE_MONITOR" | sed -n '2 p' | awk '{print $1;}')";
+
+[[ $(xrandr -q | grep ' connected' | wc -l) -eq 1 ]] \
+	&& xrandr -q | grep "$NATIVE_MONITOR connected" | grep -q '3840' \
+	&& NATIVE_MODE='2880x1620';
 
 xrandr\
 	--output "$NATIVE_MONITOR" --primary\
 	--rotate normal\
 	--pos 0x0\
-	--mode $(xrandr | grep -A 2 "$NATIVE_MONITOR" | sed -n '2 p' | awk '{print $1;}')\
+	--mode "$NATIVE_MODE"\
 	;
 
 for display in $(xrandr | grep connect | awk '{print $1;}' | grep -v "$NATIVE_MONITOR"); do
