81ee2bcca4f4c573bd0d025fc951203e83923a9a
Wryn Wagner <wagner.wryn@gmail.com>
2021-02-23 15:38:03 -0700

    added mod+shift+enter to open terminal with alternate color theme

---
+++ bin/desktop/kitty/alternate-terminal.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CONFIG_DIR="$HOME/.wryn/config/kitty"
+
+[ -f "$CONFIG_DIR/temp.conf" ] && return 1 # race condition lock
+
+mv "$CONFIG_DIR/theme.conf" "$CONFIG_DIR/temp.conf"
+mv "$CONFIG_DIR/alternate.conf" "$CONFIG_DIR/theme.conf"
+
+i3-sensible-terminal &
+sleep 0.1
+
+mv "$CONFIG_DIR/theme.conf" "$CONFIG_DIR/alternate.conf"
+mv "$CONFIG_DIR/temp.conf" "$CONFIG_DIR/theme.conf"
