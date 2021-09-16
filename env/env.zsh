5df7a59081ad66ee7b3eed344b3b9ebb36517b83
Wryn Wagner <wagner.wryn@gmail.com>
2021-09-16 12:13:05 -0600

    Added external plugins as submodules

--- env/env.zsh
+++ env/env.zsh
@@ -1,20 +1,37 @@
-# script path references
+#####################################################################
+### Path References #################################################
+#####################################################################
 DOTWRYN="$HOME/.wryn"
-SCHOOL_PATH="$HOME/School"
-
 SFX_PATH="$HOME/Media/sfx"
 WALLPAPER_PATH="$HOME/Pictures/bg"
+
+#####################################################################
+### Applications / Application Settings #############################
+#####################################################################
 
 # ordered from least-preferred to most-preferred
 PREFERRED_EDITOR=(vi vim)
 
-# plays audio files located throughout the system
+# should play an audio file argument
 MEDIA_ENGINE='canberra-gtk-play -f'
 
-# default applications
 WEBBROWSER='google-chrome-stable'
-
 TMUX_DEFAULT_SESSION_NAME='main'
 
-# external plugins
-EXTERNAL_PLUGIN_LIST=()
+#####################################################################
+### External Plugins / Settings #####################################
+#####################################################################
+
+EXTERNAL_PLUGIN_LIST=(
+	"$DOTWRYN/zsh/plugins/z/z.sh"
+	'/usr/share/fzf/key-bindings.zsh'
+	'/usr/share/fzf/completion.zsh'
+	"$DOTWRYN/zsh/plugins/fzf-tab/fzf-tab.plugin.zsh"
+	)
+
+FZF_DEFAULT_OPTS='--reverse'
+FZF_DEFAULT_COMMAND='rg --files'
+
+zstyle ':fzf-tab:*' accept-line enter
+zstyle ':fzf-tab:*' fzf-bindings 'space:accept' ';:toggle'
+zstyle ':fzf-tab:*' continuous-trigger '/'
