fdb6f3d00a21c3aaa4d71a282c777bb34c6d4bf4
yage <wagner.wryn@gmail.com>
2022-02-06 00:45:04 -0700

    Extracted vim installation functions for external use

--- setup/vim.setup.zsh
+++ setup/vim.setup.zsh
@@ -4,15 +4,12 @@ source "$DOTWRYN_PATH/setup/vim.compile.zsh"
 
 function SETUP__VIM() {
 	STATUS 'starting vim setup'
-	VIM__COMPILE_FROM_SOURCE
+	"$DOTWRYN_PATH/bin/vim/compile"
 	VIM__SOURCE_RC
-	VIM__INSTALL_VUNDLE_PLUGINS
+	"$DOTWRYN_PATH/bin/vim/install-plugins"
 	VIM__CREATE_PANE_DEFAULT_APP
 	STATUS 'finished vim setup'
 }
-
-VIM__VUNDLE_TARGET='https://github.com/VundleVim/Vundle.vim.git'
-VIM__VUNDLE_LOCAL="$HOME/.vim/bundle/Vundle.vim"
 
 #####################################################################
 
@@ -27,32 +24,6 @@ function VIM__SOURCE_RC() {
 		echo $SOURCE_LINE >> $LOCAL_VIMRC \
 			&& OK || WARN
 	}
-}
-
-function VIM__INSTALL_VUNDLE_PLUGINS() {
-	[ ! -d "$HOME/.vim/bundle/Vundle.vim" ] && {
-		CHECK 'installing Vundle.vim'
-		git clone $VIM__VUNDLE_TARGET $VIM__VUNDLE_LOCAL >>$LOG 2>&1 \
-			&& OK || { WARN; return 1; }
-	} || {
-		CHECK 'updating Vundle.vim'
-		local PREV_DIR=$(pwd)
-		cd $VIM__VUNDLE_LOCAL >>$LOG 2>&1 \
-			&& git pull >>$LOG 2>&1 \
-			&& OK || WARN 'unable to pull latest Vundle.vim'
-
-		cd $PREV_DIR >>$LOG 2>&1
-	}
-
-	STATUS 'installing Vundle.vim plugins'
-	vim +PluginInstall +qall \
-		&& SUCCESS 'successfully installed Vundle.vim plugins' \
-		|| WARN 'failed to install one or more Vundle.vim plugins' \
-		;
-
-	CHECK 'building plugins (this may take a minute)'
-	$DOTWRYN_PATH/bin/vim/rebuild-plugins >>$LOG 2>&1\
-		&& OK || WARN 'retry plugin build manually'
 }
 
 function VIM__CREATE_PANE_DEFAULT_APP() {
